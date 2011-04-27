525c086288273b7ba68e2efdf9510ad5dc14a6cf
chris_smith96 <chris_smith96@hotmail.com>
2011-04-27 23:11:39 +0000

    Fixed Issues with Error Spam by Removing use of SSI

--- TEA_xmlhttp.php
+++ TEA_xmlhttp.php
@@ -1,36 +1,22 @@
 <?php
 
-$ssi_guest_access = TRUE;
-$txt['tea_tea'] = 'Temars Eve API';
-if (file_exists('SSI.php') && !defined('SMF'))
-	require_once('SSI.php');
+$txt['tea_getchar'] = 'Get Characters';
 
-require_once("Sources/TEA.php");
-$chars = $tea -> get_characters($_GET['userid'], $_GET['api']);
+require_once("Sources/TEAC.php");
+$teac = new TEAC;
+$chars = $teac -> get_api_characters($_GET['userid'], $_GET['api']);
 
 if(!empty($chars))
 {
-	//if($_GET['page'] == 'settings')
-		echo '<select name="tea_charid" id="tea_charid" >';
-	//else
-	//	echo '<select name="tea_char">';
+	echo '<select name="tea_charid" id="tea_charid" >';
 	foreach($chars as $char)
 	{
-	//	if($_GET['page'] == 'settings')
-			echo '<option value="'.$char['charid'].'">'.$char['name'].'</option>';
-	//	else
-	//	{
-	//		if($modSettings["tea_corptag_options"] == 2)
-	//			$name = '['.$char['ticker'].'] '.$char['name'];
-	//		else
-	//			$name = $char['name'];
-	//		echo '<option value="'.$name.'">'.$name.'</option>';
-	//	}
+		echo '<option value="'.$char['charid'].'">'.$char['name'].'</option>';
 	}
 }
 else
 {
-	$error = $tea -> get_error($tea -> data);
+	$error = $teac -> get_error($teac -> data);
 	echo 'Error '.$error[0].' ('.$error[1].')<Br><select name="tea_char"><option value="-">-</option>';
 }
 echo '</select> <button type="button" onclick="javascript: getchars()">'.$txt['tea_getchar'].'</button>';
