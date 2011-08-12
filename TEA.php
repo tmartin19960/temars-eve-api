d4a60126bab147eb72c5ce82beab82c4e0150a3b
temar84 <temar84@gmail.com>
2011-08-12 16:13:20 +0000

    Fixed issue with tea function being called in other areas of profile

--- TEA.php
+++ TEA.php
@@ -23,7 +23,7 @@ class TEA extends TEAC
 		$this -> smcFunc = &$smcFunc;
 		$this -> settings = &$settings;
 
-		$this -> version = "1.2.1.153";
+		$this -> version = "1.2.1.154";
 
 		$permissions["tea_view_own"] = 1;
 		$permissions["tea_view_any"] = 0;
